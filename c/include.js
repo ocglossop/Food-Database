223410644dd7d9d466efe52f0feabdc8b5e8ca18
ocglossop <ocglossop@student.christscollege.com>
2023-06-07 12:21:34 +1200

    Added the 'let' keyword to some variables to prevent memory leaks.

--- c/include.js
+++ c/include.js
@@ -1,6 +1,6 @@
 //Based on an example from W3Schools
 function includeHTML() { //Function to allow for HTML elements to be included
-    var i, x, elements;
+    let i, x, elements;
 
     elements = document.getElementsByClassName('include-html'); //Gets all the elements in the 'include-html' class.
     max = elements.length;
@@ -9,7 +9,7 @@ function includeHTML() { //Function to allow for HTML elements to be included
         ref = x.getAttribute('data-include-src'); //Gets the source of the element to include
         if (ref) { //Ensures that the target element has a source
             //Sets up an HTML request to get the source element
-            xhttp = new XMLHttpRequest(); 
+            let xhttp = new XMLHttpRequest(); 
             xhttp.onload = function() {
                 if (this.status == 200) {
                     x.innerHTML = this.responseText; //Checks that the data has been received
